11baa72d89693426b75f1ba0ee30b38d21cae59f
Nisha Patil <nisha_patil@persistent.com>
2025-01-09 06:40:19 +0530

    fourth

---
+++ calculator1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+num1=$1
+num2=$2
+sum=$(( num1 + num2 ))
+sub=$(( num1 - num2 ))
+mul=$(( num1 * num2 ))
+div=$(( num1 / num2 ))
+echo "sum=$sum"
+echo "sub=$sub"
+echo "mul=$mul"
+echo "div=$div"
+
